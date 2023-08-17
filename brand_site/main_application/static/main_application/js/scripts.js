75c4a3b4ef9738aab03e3f98c4eb55944820ad41
ArtemSheptalin <artemsheptalinit@gmail.com>
2023-08-17 21:11:14 +0300

    добавлено переделанное верхнее меню

--- brand_site/main_application/static/main_application/js/scripts.js
+++ brand_site/main_application/static/main_application/js/scripts.js
@@ -1,3 +1,22 @@
+// СКРИПТ ВЫПАДАЮЩЕГО МЕНЮ
+
+const menu = document.querySelector("#mouse-menu");
+const dropDownMenu = document.getElementById("drop-down");
+const body = document.getElementsByTagName("body")[0];
+
+menu.addEventListener('mouseover', () => {
+    dropDownMenu.style.display = "block";
+    dropDownMenu.style.zIndex = "1";
+    dropDownMenu.style.backgroundColor = "#fafafa";
+});
+
+dropDownMenu.addEventListener('mouseleave', () => {
+    dropDownMenu.style.display = "none";
+});
+
+menu.addEventListener('mouseleave', () => {
+    dropDownMenu.style.display = "none";
+});
 
 
   
