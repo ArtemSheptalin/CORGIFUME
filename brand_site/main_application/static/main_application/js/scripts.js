7a521c8662c1aac20cc888791a0b5d4c9cbc8237
ArtemSheptalin <artemsheptalinit@gmail.com>
2023-08-21 17:52:23 +0300

    внесены корректировки страниц

--- brand_site/main_application/static/main_application/js/scripts.js
+++ brand_site/main_application/static/main_application/js/scripts.js
@@ -1,22 +1,23 @@
 // СКРИПТ ВЫПАДАЮЩЕГО МЕНЮ
 
-const menu = document.querySelector("#mouse-menu");
-const dropDownMenu = document.getElementById("drop-down");
-const body = document.getElementsByTagName("body")[0];
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector("mouse-menu");
+    const dropDownMenu = document.getElementById("drop-down");
+    const body = document.getElementsByTagName("body")[0];
 
-menu.addEventListener('mouseover', () => {
-    dropDownMenu.style.display = "block";
-    dropDownMenu.style.zIndex = "1";
-    dropDownMenu.style.backgroundColor = "#fafafa";
-});
+    menu.addEventListener('mouseover', () => {
+      dropDownMenu.style.display = "block";
+      dropDownMenu.style.zIndex = "1";
+      dropDownMenu.style.backgroundColor = "#fafafa";
+    });
 
-dropDownMenu.addEventListener('mouseleave', () => {
-    dropDownMenu.style.display = "none";
-});
+    dropDownMenu.addEventListener('mouseleave', () => {
+      dropDownMenu.style.display = "none";
+    });
 
-menu.addEventListener('mouseleave', () => {
-    dropDownMenu.style.display = "none";
-});
-
+    menu.addEventListener('mouseleave', () => {
+      dropDownMenu.style.display = "none";
+    });
+})
 
   
