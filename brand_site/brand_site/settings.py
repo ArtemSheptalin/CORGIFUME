361bdeaf95b079b7ee386d5c32fff4b9b833607a
Artem Sheptalin <shuldar001@gmail.com>
2024-03-05 01:03:52 +0300

    Все тоже самое, что и впредыдущем коммите + переделана база данных

--- brand_site/brand_site/settings.py
+++ brand_site/brand_site/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = "brand_site.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "ttt_base",
+        "NAME": "corgifume_server_db",
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "localhost",
